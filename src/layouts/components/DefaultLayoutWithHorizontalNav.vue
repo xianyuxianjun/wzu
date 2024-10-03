b1b534b5eee9829160bffacd6558eb4205277f6c
咸鱼仙君 <1796792003@qq.com>
2024-10-03 16:23:12 +0800

    first commit

---
+++ src/layouts/components/DefaultLayoutWithHorizontalNav.vue
@@ -0,0 +1,90 @@
+<script setup>
+import navItems from '@/navigation/horizontal'
+import { themeConfig } from '@themeConfig'
+
+// Components
+import Footer from '@/layouts/components/Footer.vue'
+import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
+import UserProfile from '@/layouts/components/UserProfile.vue'
+import NavBarI18n from '@core/components/I18n.vue'
+import { HorizontalNavLayout } from '@layouts'
+import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
+
+// SECTION: Loading Indicator
+const isFallbackStateActive = ref(false)
+const refLoadingIndicator = ref(null)
+
+watch([
+  isFallbackStateActive,
+  refLoadingIndicator,
+], () => {
+  if (isFallbackStateActive.value && refLoadingIndicator.value)
+    refLoadingIndicator.value.fallbackHandle()
+  if (!isFallbackStateActive.value && refLoadingIndicator.value)
+    refLoadingIndicator.value.resolveHandle()
+}, { immediate: true })
+// !SECTION
+</script>
+
+<template>
+  <HorizontalNavLayout :nav-items="navItems">
+    <!-- 👉 navbar -->
+    <template #navbar>
+      <RouterLink
+        to="/"
+        class="app-logo"
+      >
+        <VNodeRenderer :nodes="themeConfig.app.logo" />
+
+        <h1 class="app-logo-title leading-normal">
+          {{ themeConfig.app.title }}
+        </h1>
+      </RouterLink>
+      <VSpacer />
+
+      <NavBarI18n
+        v-if="themeConfig.app.i18n.enable && themeConfig.app.i18n.langConfig?.length"
+        :languages="themeConfig.app.i18n.langConfig"
+      />
+
+      <NavbarThemeSwitcher class="me-2" />
+      <UserProfile />
+    </template>
+
+    <AppLoadingIndicator ref="refLoadingIndicator" />
+
+    <!-- 👉 Pages -->
+    <RouterView v-slot="{ Component }">
+      <Suspense
+        :timeout="0"
+        @fallback="isFallbackStateActive = true"
+        @resolve="isFallbackStateActive = false"
+      >
+        <Component :is="Component" />
+      </Suspense>
+    </RouterView>
+
+    <!-- 👉 Footer -->
+    <template #footer>
+      <Footer />
+    </template>
+
+    <!-- 👉 Customizer -->
+    <!-- <TheCustomizer /> -->
+  </HorizontalNavLayout>
+</template>
+
+<style lang="scss" scoped>
+.app-logo {
+  display: flex;
+  align-items: center;
+  column-gap: 0.5rem;
+
+  .app-logo-title {
+    font-size: 1.25rem;
+    font-weight: 600;
+    line-height: 1.75rem;
+    text-transform: capitalize;
+  }
+}
+</style>
