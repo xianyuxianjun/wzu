1dd9bee91878c42454b748b43fa9109004bfb130
咸鱼仙君 <1796792003@qq.com>
2024-10-09 15:00:22 +0800

    2024.10.9

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,4 @@
 <template>
-  <div>
-    <VCard
-      class="mb-6"
-      title="Kick start your project 🚀"
-    >
-      <VCardText>All the best for your new project.</VCardText>
-      <VCardText>
-        Please make sure to read our <a
-          href="https://demos.pixinvent.com/materialize-vuejs-admin-template/documentation/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="text-decoration-none"
-        >
-          Template Documentation
-        </a> to understand where to go from here and how to use our template.
-      </VCardText>
-    </VCard>
-
-    <VCard title="Want to integrate JWT? 🔒">
-      <VCardText>We carefully crafted JWT flow so you can implement JWT with ease and with minimum efforts.</VCardText>
-      <VCardText>Please read our  JWT Documentation to get more out of JWT authentication.</VCardText>
-    </VCard>
-  </div>
+  <a href="https://demos.pixinvent.com/materialize-vuejs-admin-template/documentation/guide/">模板文档</a>
+  <a href="https://vuetifyjs.com/zh-Hans/getting-started/installation/#section-5b8988c5">框架文档</a>
 </template>
